57b25e1663cfe23cd066a15e754b59fb84b8c48b
jpuigbelloli <jpuigbelloli@alumno.unlam.edu.ar>
2023-10-19 18:58:24 -0300

    vista listar-provincia lista para mostrar

--- src/main/java/com/tallerwebi/presentacion/ControladorViaje.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorViaje.java
@@ -3,12 +3,10 @@ package com.tallerwebi.presentacion;
 import com.tallerwebi.dominio.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.List;
 
