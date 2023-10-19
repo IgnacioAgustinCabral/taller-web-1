49b4fd71d32ddd4b4c639636653d3a15a7523c67
jpuigbelloli <jpuigbelloli@alumno.unlam.edu.ar>
2023-10-19 20:16:53 -0300

    faltaab importar

--- src/main/java/com/tallerwebi/presentacion/ControladorViaje.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorViaje.java
@@ -7,6 +7,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.List;
 
