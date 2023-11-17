5416b57ceaaa61abce3b80d6f35c8a31af9656f6
jpuigbelloli <jpuigbelloli@alumno.unlam.edu.ar>
2023-11-17 17:49:34 -0300

    modifico datasql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -87,6 +87,7 @@ VALUES (2,null,STR_TO_DATE('15/03/2024', '%d/%m/%Y'),3 , 5, 2),
        (3,null,STR_TO_DATE('22/12/2023', '%d/%m/%Y'),15,8,5),
        (3,null,STR_TO_DATE('22/12/2023', '%d/%m/%Y'),15,8,5),
        (3,null,STR_TO_DATE('10/11/2023', '%d/%m/%Y'),1,7,6);
+
 /*
 INSERT INTO Viaje_Compa (id_viaje, id_usuario)
 VALUES
